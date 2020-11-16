30d7d30faf4ab42561ee4f1f8a199cc61301b616
Drew Thompson <drewallynthompson@gmail.com>
2020-11-15 20:16:00 -0700

    minor class fixes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,3 @@
 class Recipe < ActiveRecord::Base
-    
+    validates_presence_of :name, :grain, :yeast, :hops, :malt
 end
--- db/migrate/20201115073842_create_recipes.rb
+++ db/migrate/20201115073842_create_recipes.rb
@@ -2,7 +2,11 @@ class CreateRecipes < ActiveRecord::Migration
   def change
     create_table :recipes do |t|
       t.string :name
-      t.string :ingredients
+      t.string :hops
+      t.string :malt
+      t.string :yeast
+      t.string :grain
+      t.string :other_ingredients
       t.belongs_to :user
     end
   end
