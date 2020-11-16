86f6cb06f1bfdc075640035b5294629cd4e5ab84
Drew Thompson <drewallynthompson@gmail.com>
2020-11-15 21:12:38 -0700

    finished  test

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,3 @@
 class Recipe < ActiveRecord::Base
     validates_presence_of :name, :grain, :yeast, :hops, :malt
-
-    this porgarm is wack yo
 end
--- spec/application_controller_spec.rb
+++ spec/application_controller_spec.rb
@@ -5,9 +5,11 @@ def app
 end
 
 describe ApplicationController do
-  it "responds with a welcome message" do
-    get '/'
-    expect(last_response.status).to eq(200)
-    expect(last_response.body).to include("Welcome to the Sinatra Template!")
+  describe "Homepage" do
+    it 'loads the homepage' do
+      get '/'
+      expect(last_response.status).to eq(200)
+      expect(last_response.body).to include("Welcome to Fwitter")
+    end
   end
 end
