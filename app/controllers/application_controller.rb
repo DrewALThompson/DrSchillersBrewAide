72b96482845312b2e40293d50e41671928acfbdf
Drew Thompson <drewallynthompson@gmail.com>
2020-11-18 18:33:44 -0700

    layout and current user test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,7 +25,7 @@ class ApplicationController < Sinatra::Base
     end
 
     def current_user
-      @current_user? @current_user : User.find(session[:id]) if session[:id]
+      @current_user ||= User.find(session[:id]) if session[:id]
     end
   end
 
